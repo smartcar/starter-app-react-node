44bd6d9c745d16eade971d8b40ec1ec11d5b2e49
Thach Do <thach.do@smartcar.com>
2023-07-14 10:11:33 -0700

    minor change to units

--- client/src/components/Properties.jsx
+++ client/src/components/Properties.jsx
@@ -27,13 +27,13 @@ const staticText = {
   longitude: (longitude) => `Long: ${longitude.toFixed(4)}...`,
   engineOil: (engineOil) => `${(engineOil * 100).toFixed()}% remaining`,
   frontLeft: (frontLeft) =>
-    `Front left: ${frontLeft.toFixed(1)} ${isMetric ? 'Kilopascal' : 'psi'}`,
+    `Front left: ${frontLeft.toFixed(1)} ${isMetric ? 'kPa' : 'psi'}`,
   frontRight: (frontRight) =>
-    `Front right: ${frontRight.toFixed(1)} ${isMetric ? 'Kilopascal' : 'psi'}`,
+    `Front right: ${frontRight.toFixed(1)} ${isMetric ? 'kPa' : 'psi'}`,
   backLeft: (backLeft) =>
-    `Back left: ${backLeft.toFixed(1)} ${isMetric ? 'Kilopascal' : 'psi'}`,
+    `Back left: ${backLeft.toFixed(1)} ${isMetric ? 'kPa' : 'psi'}`,
   backRight: (backRight) =>
-    `Back right: ${backRight.toFixed(1)} ${isMetric ? 'Kilopascal' : 'psi'}`,
+    `Back right: ${backRight.toFixed(1)} ${isMetric ? 'kPa' : 'psi'}`,
   lock: 'Lock',
   unlock: 'Unlock',
   startCharge: 'Start charge',
